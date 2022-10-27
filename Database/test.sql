b8f3e56389d03183c8b7c26ac15fb5491cb017dd
hashemkhodor <87622346+hashemkhodor@users.noreply.github.com>
2022-10-27 18:10:00 +0300

    Update test.sql

--- Database/test.sql
+++ Database/test.sql
@@ -1,8 +1,17 @@
-CREATE TABLE Users (
+CREATE TABLE UsersCredentials (
    username varchar() ,
    password varchar() ,
    email varchar() ,
    Date_of_creation timestamp ,
    Last_login timestamp,
    active boolean
+);
+
+CREATE TABLE UsersInfo(
+   username  varchar()
+   FirstName  varchar()
+   LastName  varchar()
+   Phone Number  int
+   Birthdate  DATE
+   Location varchar()
 );
