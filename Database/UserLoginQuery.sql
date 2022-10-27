bc92e0e73026e43f87ceb1c6e4d08f3e49ef6f6d
hashemkhodor <87622346+hashemkhodor@users.noreply.github.com>
2022-10-27 18:20:01 +0300

    Create UserLoginQuery.sql

---
+++ Database/UserLoginQuery.sql
@@ -0,0 +1,5 @@
+select username from UsersCredentials where username is --- ; --Query for login page
+select password from UsersCredentials where username is --- ; --Query for login page
+select email from UsersCredentials where usernames is --- ; --Query for login page
+
+
