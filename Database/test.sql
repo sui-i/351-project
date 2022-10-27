2b7491abe3db8b6b04ca40ffa3565349b92b36c0
hashemkhodor <87622346+hashemkhodor@users.noreply.github.com>
2022-10-27 18:11:54 +0300

    Update test.sql

--- Database/test.sql
+++ Database/test.sql
@@ -8,10 +8,20 @@ CREATE TABLE UsersCredentials (
 );
 
 CREATE TABLE UsersInfo(
-   username  varchar()
-   FirstName  varchar()
-   LastName  varchar()
-   Phone Number  int
-   Birthdate  DATE
+   username  varchar(),
+   FirstName  varchar(),
+   LastName  varchar(),
+   Phone Number  int,
+   Birthdate  DATE,
    Location varchar()
+);
+
+CREATE TABLE UserReservation (
+   username  varchar,
+   room_id  int ,
+   Date of Reservation  timestamp,
+   check_in  timestamp,
+   check_out  timestamp,
+   cancelled  timestamp 
+
 );
