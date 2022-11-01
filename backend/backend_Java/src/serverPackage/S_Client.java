c15da9a904c452145b9c1ea8ea8e9c6e6b4b2b61
Ahmad El Hajj <AhmadElHajj110@gmail.com>
2022-11-01 21:38:52 +0200

    updated

--- backend/backend_Java/src/serverPackage/S_Client.java
+++ backend/backend_Java/src/serverPackage/S_Client.java
@@ -7,15 +7,18 @@ import DataBasePackage.DB_API;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class S_Client implements Runnable{
+	private static void print(String s) {System.out.println("[SERVER]: " + s);}
 	private Socket socket;
 	private BufferedReader bufferedReader;
 	private BufferedWriter bufferedWriter;
 	
+	boolean isLoggedIn;
 	String clientUsername;
 	String clientEmail;
 	String clientPassword;
@@ -29,33 +32,50 @@ public class S_Client implements Runnable{
 			this.socket = socket;
 			this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			this.bufferedReader = new BufferedReader(new InputStreamReader (socket.getInputStream( )));
-			this.clientUsername = bufferedReader.readLine();
-			this.clientPassword = bufferedReader.readLine();
-			
-			
-			//TODO: add thread locks to registrations
-			if (DB_API.checkMembership(clientUsername))
-			{
-				if (!DB_API.checkLoginCredentials(clientUsername, clientPassword))
-					//TODO: Signal to the client that username or password does not exist
-					//or that email is not verified.
-					//and kill the client
-			}
-			else
-			{
-				//Ask the user if they want to register, and let them provide their email
+			isLoggedIn = false;
 				
-				int RegistrationErrorCode = DB_API.RegisterUser(clientUsername, clientEmail, clientPassword);
-				if (RegistrationErrorCode == 0) {}
-				else if (RegistrationErrorCode == 1) {}
-				else if (RegistrationErrorCode == 2)
-					 //TODO: Signal the error to the user and kill the client
-				//TODO: send a verification mail
-			}
+		} catch (IOException e){
+			closeEverything(socket,bufferedReader,bufferedWriter);
 		}
 		
 	}
-
+	
+	private static int validateRoomID(String roomID) 
+	{
+		//checks if the roomID is within format returns 1
+		//checks if the solar system is a valid returns 2
+		//checks if the Planet is valid returns 3
+		//checks if the Hotel is valid returns 4
+		//checks if the room is within range of rooms 5
+		//if all is well return 0
+	}
+	public int reserve(String roomID, int time)
+	{
+		if (!isLoggedIn)
+			return -1;
+		int roomErrorCode = S_Client.validateRoomID(roomID);
+		if (roomErrorCode!=0)
+		{
+			return roomErrorCode;
+		}
+		//if room reserved is already reserved at hour, return 6
+		
+		//if all is well, reserve the room and return 0
+	}
+	public int unReserve(String roomID, int time)
+	{
+		if (!isLoggedIn)
+			return -1;
+		int roomErrorCode = S_Client.validateRoomID(roomID);
+		if (roomErrorCode!=0)
+		{
+			return roomErrorCode;
+		}
+		//if this room is not reserved at the certain time by this user, return 6
+		
+		//if all is well, remove reserve the room and return 0
+	}
+	
 	@Override
 	public void run() {
 		
