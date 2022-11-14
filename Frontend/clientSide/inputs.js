e7870646881e4b97f68dbacea2b6ff3a076557cc
Mounif <mounifelkhatib@hotmail.com>
2022-11-14 18:52:29 +0200

    Implemented getters for all inputs

--- Frontend/clientSide/inputs.js
+++ Frontend/clientSide/inputs.js
@@ -1,41 +1,41 @@
 // this function stores the username that the user inputs upon login
 function getUsername() {
   var username = document.getElementById("username").value;
-  return username;
   //alert(username);
+  return username;
 }
 
 // this function stores the password that the user inputs upon login
 function getPassword() {
   var password = document.getElementById("password").value;
-  return password;
   //alert(password);
+  return password;
 }
 
 // this function stores the username that the user inputs upon registering
 function getRegistrationUsername() {
   var regUser = document.getElementById("regUser").value;
-  alert(regUser);
+  //alert(regUser);
   return regUser;
 }
 
 // this function stores the email that the user inputs upon registering
 function getRegistrationEmail() {
   var regEmail = document.getElementById("regEmail").value;
-  alert(regEmail);
+  //alert(regEmail);
   return regEmail;
 }
 
 // this function stores the password that the user inputs upon registering
 function getRegistrationPassword() {
   var regPass = document.getElementById("regPass").value;
-  alert(regPass);
+  //alert(regPass);
   return regPass;
 }
 
 // this function stores the re-entered password that the user inputs upon registering
 function getReEnteredPassword() {
   var regRePass = document.getElementById("regRePass").value;
-  alert(regRePass);
+  //alert(regRePass);
   return regRePass;
 }
