31f06343664a7d6bcc69f23444fdc4f1b9b80425
Mounif <mounifelkhatib@hotmail.com>
2022-11-26 01:59:22 +0200

    idk

--- Frontend/clientSide.js
+++ Frontend/clientSide.js
@@ -4,9 +4,9 @@ var form = document.getElementById("form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (username.value && password.value) {
-    var string = String(username.value) + "," + String(password.value);
-    var x = JSON.stringify(string);
-    socket.emit("output", string);
+    //var string = String(username.value) + "," + String(password.value);
+    // var x = JSON.stringify(string);
+    socket.emit("output", username.value, password.value);
     username.value = "";
     password.value = "";
   }
