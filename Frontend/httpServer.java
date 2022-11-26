28d7c603a59af55e5407c5578180e7eacedcb9d9
Mounif <mounifelkhatib@hotmail.com>
2022-11-26 17:55:58 +0200

    implemented server i want to die

--- Frontend/httpServer.java
+++ Frontend/httpServer.java
@@ -1,10 +1,16 @@
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
@@ -16,17 +22,24 @@ public class httpServer {
 	public static void main(String[] args) throws Exception {
 		HttpServer httpserver = HttpServer.create(new InetSocketAddress(3000), 0);
 		HttpContext hc = httpserver.createContext("/");
-		httpserver.createContext("/static", new StaticFileHandler("/file:///", "C:/Users/Mouni/Desktop/351/351-project/Frontend", "loginPage_stylesheet.css"));
+
 		hc.setHandler(new HttpHandler() {
 			@Override
 			public void handle(HttpExchange exchange) throws IOException {
 				Headers h = exchange.getResponseHeaders();
+				
 				String line;
 				String r = "";
 				// System.out.println(exchange.getRequestBody());
-
+				
 				File newFile = new File("C:/Users/Mouni/Desktop/351/351-project/Frontend/loginPage.html");
+				BufferedImage logo = ImageIO.read(new File("C:/Users/Mouni/Desktop/351/351-project/Frontend/Items/Hotel logo.png"));
+				ByteArrayOutputStream logoBytes = new ByteArrayOutputStream();
+				ImageIO.write(logo, "png", logoBytes);
+				byte[] data = logoBytes.toByteArray();
+				String s = new String(data, StandardCharsets.UTF_8);
 				BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(newFile)));
+				
 				while ((line = br.readLine()) != null) {
 					//System.out.println(line);
 					r += line;
@@ -34,10 +47,12 @@ public class httpServer {
 				h.add("Content-Type", "text/html");
 
 				exchange.sendResponseHeaders(200, r.length());
-
+				//exchange.sendResponseHeaders(200, 0);
 				OutputStream os = exchange.getResponseBody();
+				System.out.println(s);
 				br.close();
 				os.write(r.getBytes());
+				os.write(s.getBytes());
 				os.close();
 			}
 		});
