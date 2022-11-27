55b4c9982ea9c0a128fd9030bf506a83b632e63a
Mounif <mounifelkhatib@hotmail.com>
2022-11-27 20:28:56 +0200

    sui

--- Frontend/httpServer.java
+++ Frontend/httpServer.java
@@ -5,11 +5,19 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Handler;
 
 import javax.imageio.ImageIO;
+import javax.swing.text.Document;
+
 import java.awt.image.BufferedImage;
 
 import com.sun.net.httpserver.HttpServer;
@@ -20,45 +28,48 @@ import com.sun.net.httpserver.Headers;
 
 public class httpServer {
 	public static void main(String[] args) throws Exception {
-		HttpServer httpserver = HttpServer.create(new InetSocketAddress(3000), 0);
-		HttpContext hc = httpserver.createContext("/");
 
+		HttpServer httpserver = HttpServer.create(new InetSocketAddress(3000), 0);
+
+		HttpContext hc = httpserver.createContext("/");
 		hc.setHandler(new HttpHandler() {
 			@Override
 			public void handle(HttpExchange exchange) throws IOException {
-				Headers h = exchange.getResponseHeaders();
-				
-				String line;
-				String r = "";
-				// System.out.println(exchange.getRequestBody());
-				
-				File newFile = new File("C:/Users/Mouni/Desktop/351/351-project/Frontend/loginPage.html");
-				BufferedImage logo = ImageIO.read(new File("C:/Users/Mouni/Desktop/351/351-project/Frontend/Items/Hotel logo.png"));
-				ByteArrayOutputStream logoBytes = new ByteArrayOutputStream();
-				ImageIO.write(logo, "png", logoBytes);
-				byte[] data = logoBytes.toByteArray();
-				String s = new String(data, StandardCharsets.UTF_8);
-				BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(newFile)));
-				
-				while ((line = br.readLine()) != null) {
-					//System.out.println(line);
-					r += line;
-				}
-				h.add("Content-Type", "text/html");
 
-				exchange.sendResponseHeaders(200, r.length());
-				//exchange.sendResponseHeaders(200, 0);
+				FileInputStream fis = new FileInputStream("loginPage.html");
+				FileInputStream fis1 = new FileInputStream("loginPage_stylesheet.css");
+				exchange.sendResponseHeaders(200, 0);
+				exchange.getResponseHeaders().add("Content-Type", "text/html");
+				// exchange.sendResponseHeaders(200, 0);
+				// exchange.getResponseHeaders().add("Content-Type", "text/css");
 				OutputStream os = exchange.getResponseBody();
-				System.out.println(s);
-				br.close();
-				os.write(r.getBytes());
-				os.write(s.getBytes());
+				copyStream(fis, os);
+				copyStream(fis1, os);
 				os.close();
+
+				/*
+				 * fis1.readAllBytes();
+				 * copyStream(fis1, os);
+				 * copyStream(fis, os);
+				 * os.close();
+				 * fis.close();
+				 * fis1.close();
+				 */
+
 			}
 		});
-		
+
 		httpserver.setExecutor(null);
 		httpserver.start();
+
+	}
+
+	private static void copyStream(InputStream is, OutputStream os) throws IOException {
+		byte[] buf = new byte[4096];
+		int n;
+		while ((n = is.read(buf)) >= 0) {
+			os.write(buf, 0, n);
+		}
 	}
 
 }
