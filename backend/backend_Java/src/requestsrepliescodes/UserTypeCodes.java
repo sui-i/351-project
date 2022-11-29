0afe95e7e257ee1b30fbf199e9a138c9d401538e
hashemkhodor <hashemkhoder1@gmail.com>
2022-11-29 16:41:40 +0200

    Minor changes

--- backend/backend_Java/src/requestsrepliescodes/UserTypeCodes.java
+++ backend/backend_Java/src/requestsrepliescodes/UserTypeCodes.java
@@ -1,11 +1,14 @@
 package requestsrepliescodes;
 
 public enum UserTypeCodes {
-    
+    DuplicatedUsers(-2),
+    NotFound(-1),
     Admin(0),
     VerifiedUser(1),
     NonVerifiedUser(2),
-    NotLoggedIn(3);
+    NotLoggedIn(3),
+
+    InternalError(500);
 
     public final int ID;
     private UserTypeCodes(int ID){
