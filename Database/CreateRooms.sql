de69edbe9837aa4a558eec77cfe21784c4b72924
hashemkhodor <hashemkhoder1@gmail.com>
2022-12-02 21:47:53 +0200

    testing

---
+++ Database/CreateRooms.sql
@@ -0,0 +1,22 @@
+
+
+/*
+INSERT INTO planet_info (simple_id,simple_name) VALUES 
+('EART','The Earth'),
+('MARS','Mars'),
+('JUPT','Jupiter'),
+('DADD','The Daddy');
+
+INSERT INTO solar_system_info (simple_id,simple_name) VALUES 
+('MILK','The Milky Way'),
+('ANDR','Andromidaddy');
+
+INSERT INTO hotel_info (simple_id,simple_name,number_of_floors,number_of_rooms) VALUES 
+('SHER','Sheraton',5,10),
+('HABB','Habibi',22,22);
+
+INSERT INTO room_info (room_id,room_type,num_of_beds,floor,price_per_night) VALUES
+('MILKMARSSHER1001','01',2,4,11.2),
+('ANDRDADDHABB2002','01',1,2,102);
+
+*/
